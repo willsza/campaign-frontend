bc15bc783892f821dc6d082c0dca124cbad29721
Will <william.souza@raroprestador.com.br>
2024-06-25 19:36:34 -0300

    chore: add shadcnui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello World!</h1>
+      <Button>Click me</Button>
     </main>
   );
 }
