dc551c5fd9db9c6ff8532e9e3619e7b05f16b7bc
Will <william.souza@raroprestador.com.br>
2024-06-25 22:19:01 -0300

    feat: add dashboard components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Inter as FontSans } from "next/font/google";
 
 import "./globals.css";
 
-import Header from "@/components/layout/Header";
-import Sidebar from "@/components/layout/Sidebar";
 import { cn } from "@/lib/utils";
 
 const fontSans = FontSans({
@@ -30,11 +28,7 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        <Header />
-        <section className="flex h-screen overflow-hidden">
-          <Sidebar />
-          <main className="flex-1 overflow-hidden pt-16">{children}</main>
-        </section>
+        {children}
       </body>
     </html>
   );
