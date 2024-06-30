3113d674016e72545129863d668e2ee00cf044b8
Will <william.souza@raroprestador.com.br>
2024-06-30 14:34:07 -0300

    refactor: improviments to campaign page

--- src/app/(dashboard)/dashboard/campaigns/page.tsx
+++ src/app/(dashboard)/dashboard/campaigns/page.tsx
@@ -1,3 +1,5 @@
+import { Fragment } from "react";
+
 import { campaignList } from "@/actions/campaign";
 import { categoryList } from "@/actions/category";
 import BreadCrumb from "@/components/layout/Breadcrumb";
@@ -10,11 +12,11 @@ export default async function page() {
   const categories = await categoryList();
 
   return (
-    <>
+    <Fragment>
       <div className="flex-1 space-y-4  p-4 pt-6 md:p-8">
         <BreadCrumb items={breadcrumbItems} />
         <CampaignClient data={data} categories={categories} />
       </div>
-    </>
+    </Fragment>
   );
 }
